6587ecdd0633e76677b76cd877de1115503643fe
bcersows <dev@bcersows.de>
2020-11-27 15:31:02 +0100

    try to find new blog posts by parsing XML

--- js/App.js
+++ js/App.js
@@ -32,10 +32,61 @@ document.addEventListener("DOMContentLoaded", function () {
           document.getElementById("version-info").textContent = text;
         });
       } else {
-        console.warn("Could not load the Git info: " + response.status + ": " + response.statusText);
+        console.warn(
+          `Could not load the Git info: ${response.status}: ${response.statusText}`
+        );
       }
     })
     .catch(function (err) {
       console.error("Error when loading the Git info: " + err.message);
+    });
+
+  /**
+   * Parse the given RSS XML and returns if found any new blog entries.
+   * @param {String} xml the RSS feed XML
+   * @returns if found any
+   */
+  function extractRssFeedItem(xml) {
+    // create parser and parse input
+    var parser = new DOMParser();
+    var xmlDoc = parser.parseFromString(xml, "text/xml");
+
+    // threshold is one week
+    const threshold = new Date().getTime() - 604800000;
+
+    // select all blog items
+    const items = xmlDoc.querySelectorAll("item");
+
+    console.debug(`Found ${items.length} blogs.`);
+
+    // iterating over all items, find one whose pub data is within the threshold
+    for (const el of items) {
+      const pubTime = new Date(el.querySelector("pubDate").innerHTML).getTime();
+
+      // if found one, return true
+      if (threshold < pubTime) {
+        console.debug("New blog!");
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  fetch("https://blog.bcersows.de/index.php?/feeds/index.rss2")
+    .then(function (response) {
+      if (response.ok) {
+        var feedXml = response.text();
+        var hasNewBlog = extractRssFeedItem(feedXml);
+
+        console.log("Has new blog entries: " + hasNewBlog);
+      } else {
+        console.warn(
+          `Could not load the RSS feed: ${response.status}: ${response.statusText}`
+        );
+      }
+    })
+    .catch(function (err) {
+      console.error("Error when loading the RSS feed: " + err.message);
     });
 });
